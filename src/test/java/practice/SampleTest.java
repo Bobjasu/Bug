6f384140ad21fc87268f4f8f4491fd55c3f60e08
Bobjasu <utsab12.bcet@gmail.com>
2023-03-27 10:43:40 +0530

    1st commit

---
+++ src/test/java/practice/SampleTest.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test(groups="SmokeTest")
+	public void m1() {
+		System.out.println("m1 is running");
+	}
+//push
+}
